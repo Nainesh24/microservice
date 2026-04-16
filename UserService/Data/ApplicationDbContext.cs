b4854a16864f1dc5306162cc02f6a4f863c08b34
naineshc <chovatiyanainesh24@gmail.com>
2026-04-16 14:38:40 +0530

    first commit and commit the userservice

---
+++ UserService/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Model;
+
+namespace UserService.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+        }
+        DbSet<User> Users { get; set; }
+    }
+}
---
+++ UserService/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserService.Model
+{
+    [Table("user")]
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ModifiedAt { get; set; }
+        public string Status { get; set; } = "A";
+    }
+}
